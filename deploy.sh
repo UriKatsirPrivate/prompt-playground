43318cdbfbaf4dd69a08e132464a4bccbd37c957
Uri Katsir <46541339+UriKatsirPrivate@users.noreply.github.com>
2024-03-12 16:58:13 +0200

    create artifact registry repo as part of deployment

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,7 @@ gcloud services enable run.googleapis.com --project $GOOGLE_CLOUD_PROJECT --asyn
 gcloud services enable aiplatform.googleapis.com --project $GOOGLE_CLOUD_PROJECT --async
 
 # Artifact Registry
+# gcloud artifacts repositories create $ARTIFACT_REGISTRY_NAME --location=$REGION --repository-format=docker
 gcloud builds submit --tag $REGION-docker.pkg.dev/$GOOGLE_CLOUD_PROJECT/$ARTIFACT_REGISTRY_NAME/$SERVICE_NAME:latest
 
 gcloud run deploy $SERVICE_NAME \
